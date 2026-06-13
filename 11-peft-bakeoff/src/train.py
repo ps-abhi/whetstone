e411c29e4cae884b780afa064ac5c4ef97dc6fad
ps-abhi <psabhiishek@gmail.com>
2026-06-13 14:12:15 +0530

    Added remaining peft methods and hydra integration.
    To maintain bakeoff idea, currently all configs share the same hyperparameters(with exception of prefix)

--- 11-peft-bakeoff/src/train.py
+++ 11-peft-bakeoff/src/train.py
@@ -1,5 +1,5 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from peft import LoraConfig, TaskType, get_peft_model
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from peft import LoraConfig, TaskType, get_peft_model, prepare_model_for_kbit_training, PrefixTuningConfig
 from datasets import load_dataset
 from trl import SFTTrainer, SFTConfig
 import torch
@@ -8,14 +8,7 @@ import numpy as np
 import hydra
 from omegaconf import DictConfig, OmegaConf
 
-random.seed(42)
-torch.manual_seed(42)
-np.random.seed(42)
-
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-dataset = load_dataset("HuggingFaceH4/CodeAlpaca_20K")
 
 def preprocessing(instance):
     return {
@@ -25,50 +18,60 @@ def preprocessing(instance):
         ]
     }
 
-dataset = dataset.map(preprocessing, remove_columns=["prompt", "completion"])
-train_dataset = dataset["train"]
-test_dataset = dataset["test"]
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg : DictConfig):
+    random.seed(cfg.seed)
+    torch.manual_seed(cfg.seed)
+    np.random.seed(cfg.seed)
+    
+    dataset = load_dataset(cfg.dataset_name)
+    dataset = dataset.map(preprocessing, remove_columns=["prompt", "completion"])
+    train_dataset = dataset["train"]
+    test_dataset = dataset["test"]
+    
+    tokenizer = AutoTokenizer.from_pretrained(cfg.model_name)
+    quant = cfg.peft.get("quantization")
+    if quant:
+        quant = OmegaConf.to_container(cfg.peft.quantization, resolve=True)
+        bnb_config = BitsAndBytesConfig(**quant)
+        model = AutoModelForCausalLM.from_pretrained(cfg.model_name, dtype=torch.bfloat16, quantization_config=bnb_config)
+        model = prepare_model_for_kbit_training(model)
+    else:
+        model = AutoModelForCausalLM.from_pretrained(cfg.model_name, dtype=torch.bfloat16)
 
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-4B-Instruct-2507")
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-4B-Instruct-2507", dtype=torch.bfloat16)
+    peft_method = cfg.peft.name
+    peft_kwargs = OmegaConf.to_container(cfg.peft, resolve=True)
+    peft_kwargs.pop("name")
+    peft_kwargs.pop("quantization", None)
 
-lora_config = LoraConfig(
-    r = 16,
-    lora_alpha= 32,
-    lora_dropout = 0.05,
-    target_modules="all-linear",    
-    task_type=TaskType.CAUSAL_LM
-)
+    if peft_method == "prefix":
+        peft_config = PrefixTuningConfig(task_type="CAUSAL_LM", **peft_kwargs)
+    
+    else:
+        peft_config = LoraConfig(task_type = "CAUSAL_LM", **peft_kwargs)
+        
+    model = get_peft_model(model, peft_config)
+    
+    model.print_trainable_parameters()
 
-model = get_peft_model(model, lora_config)
+    sft_kwargs = OmegaConf.to_container(cfg.train, resolve=True)
 
-model.print_trainable_parameters()
-
-
-
-
-
-
-
-args = SFTConfig(
-        learning_rate=2e-4,
-        bf16=True,
-        assistant_only_loss=True,
-        per_device_train_batch_size=2,
-        gradient_accumulation_steps=8,
-        max_steps=1000,
-        seed=42,
-        max_length=1024,
-        output_dir="./lora_train",
-        lr_scheduler_type="cosine"
+    args = SFTConfig(
+        seed = cfg.seed, 
+        report_to="wandb"
+        **sft_kwargs
     )
 
+    trainer = SFTTrainer(
+        model = model,
+        train_dataset = train_dataset,
+        args = args,
+        processing_class = tokenizer,
+    )
 
-trainer = SFTTrainer(
-    model=model,
-    train_dataset=train_dataset,
-    args = args,
-    processing_class=tokenizer,
-)
+    trainer.train()
 
-trainer.train()
+
+if __name__=="__main__":
+    main()
+
