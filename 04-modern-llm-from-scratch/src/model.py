88e755e128b82ea95d49b39b68bd4ecc4520626c
ps-abhi <psabhiishek@gmail.com>
2026-06-24 22:36:55 +0530

    (04-modern-llm-from-scratch): Attention

--- 04-modern-llm-from-scratch/src/model.py
+++ 04-modern-llm-from-scratch/src/model.py
@@ -1,3 +1,4 @@
+from numpy import sqrt
 from torch import rsqrt, mean
 import torch.nn as nn
 import torch
@@ -48,3 +49,61 @@ class RoPE(nn.Module):
         cos = self.cos[start_pos : start_pos + T]
         sin = self.sin[start_pos : start_pos + T]
         return x * cos + self.rotate_half(x) * sin  # out = x·cos + rotate_half(x)·sin
+    
+class Attention(nn.Module):
+    def __init__(self, head_dim, d_model, n_heads, n_kv_heads, max_seq_length):
+        super().__init__()
+        self.n_heads = n_heads
+        self.n_kv_heads = n_kv_heads
+        self.head_dim = head_dim
+        self.max_seq_length = max_seq_length
+        self.n_rep = n_heads // n_kv_heads
+
+        self.q_proj = nn.Linear(d_model, n_heads*head_dim, bias=False) # no additive bias
+        self.k_proj = nn.Linear(d_model, n_kv_heads*head_dim, bias=False)
+        self.v_proj = nn.Linear(d_model, n_kv_heads*head_dim, bias=False)
+        self.o_proj = nn.Linear(n_heads*head_dim, d_model, bias=False)  # 256 -> 256
+
+        self.rope = RoPE(head_dim, max_seq_length)
+    
+    def forward(self, x, start_pos=0): #x: [B, T, d_model])
+        B, T, _ = x.shape
+
+        q = self.q_proj(x) # # [B, T, 256]
+        q = q.view(B, T, self.n_heads, self.head_dim) ## [B, T, 8, 32] split that flat 256 into (heads, head_dim), then move heads next to batch
+        q = q.transpose(1, 2)       
+
+        k = self.k_proj(x)
+        k = k.view(B, T, self.n_kv_heads, self.head_dim)
+        k = k.transpose(1, 2)
+
+        v = self.v_proj(x)
+        v = v.view(B, T, self.n_kv_heads, self.head_dim)
+        v = v.transpose(1,2)
+
+        q = self.rope(q, start_pos)
+        k = self.rope(k, start_pos)
+
+        def repeat_kv(x, n_rep):
+            if n_rep == 1:
+                return x 
+            B, n_kv, T, Dh = x.shape
+            x = x[:, :, None, :, :].expand(B, n_kv, n_rep, T, Dh)
+            return x.reshape(B, n_kv * n_rep, T, Dh) # [B, n_heads, T, Dh]
+            
+        k = repeat_kv(k, self.n_rep) # # [B, 2, T, 32] -> [B, 8, T, 32]
+        v = repeat_kv(v, self.n_rep)
+
+        scores = (q @ k.transpose(-2, -1))/sqrt(self.head_dim)
+        mask = torch.triu(torch.ones(T, T, dtype=bool, device=x.device), diagonal=1)
+        scores = scores.masked_fill(mask, float("-inf"))
+
+        attention = torch.softmax(scores, dim=-1) # [B, 8, T, T]
+        out = attention @ v #  # [B, 8, T, 32]
+
+        out = out.transpose(1, 2).reshape(B, T, self.n_heads * self.head_dim)
+        return self.o_proj(out)
+
+
+attn= Attention(head_dim=32, d_model=256, n_heads=8, n_kv_heads=2, max_seq_length=64)
+print(attn(torch.randn(2, 10, 256)).shape) 
